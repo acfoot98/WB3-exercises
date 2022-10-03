d99f785423541b953d9ddcf47d2bfce7bb2d1e73
Aili Lightfoot <alightfoot@atl.yearup.org>
2022-10-03 11:31:07 -0400

    Favorite things

---
+++ simplefunctions.js
@@ -0,0 +1,17 @@
+function favoriteThings(){
+    console.log("Aili");
+    console.log("Happy Gilmore");
+    console.log("Korn", "Slipknot");
+}
+
+function whyImHere(){
+    console.log("I am interested in Computers and would like to learn more about them");
+}
+
+function favoritePlace(){
+    console.log("My favorite place is Madison, WI");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
