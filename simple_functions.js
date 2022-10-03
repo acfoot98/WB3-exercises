722e3ba687d199f0a6e6b228dafb84860f1ac4f3
Aili Lightfoot <alightfoot@atl.yearup.org>
2022-10-03 11:51:15 -0400

    Updated

---
+++ simple_functions.js
@@ -0,0 +1,19 @@
+//DEFINE THREE NAMED FUNCTIONS
+function favoriteThings(){
+    console.log("NAME: Aili Lightfoot");
+    console.log("MOVIE: Happy Gilmore");
+    console.log("BANDS: Korn", "Slipknot");
+}
+
+function whyImHere(){
+    console.log("WHY: I am interested in Computers and would like to learn more about them");
+}
+
+function favoritePlace(){
+    console.log("PLACE: My favorite place is Madison, WI");
+}
+
+//CALL ALL THREE FUNCTIONS BY NAME
+favoriteThings();
+whyImHere();
+favoritePlace();
